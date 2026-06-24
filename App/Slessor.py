1702f5482a0b101d4b35a350e521fe241b9a1f2c
Robin Spinks <uglybluecat@googlemail.com>
2026-06-24 16:25:43 +0100

    Comment unneeded debug print() statement

--- App/Slessor.py
+++ App/Slessor.py
@@ -24,26 +24,26 @@ def sudoku_row_checker(sudoku_board):
     error_coordinates = []
 
     for row in sudoku_board:
-        print(tag + f'row: {row}')
+        # print(tag + f'row: {row}')
         row_list = row.strip('()').split(',')
-        print(tag + f'row_list: {row_list}')
+        # print(tag + f'row_list: {row_list}')
 
         for i in range(1,10):
             i_count = row_list.count(str(i))
-            print(tag + f'i: {i} i_count: {i_count}')
+            # print(tag + f'i: {i} i_count: {i_count}')
 
             if i_count > 1:
                 row_index = sudoku_board.index(row)
-                print(tag + f'row_index: {row_index}')
+                # print(tag + f'row_index: {row_index}')
 
                 prev_column_index = 0
                 for j in range (i_count):
                     column_index = row_list.index(str(i), prev_column_index)
-                    print(tag + f'column_index: {column_index}')
+                    # print(tag + f'column_index: {column_index}')
                     prev_column_index = column_index + 1
                     error_coordinates.append([row_index + 1, column_index + 1])
 
-    print(tag + f'error_coordinates: {error_coordinates}')
+    # print(tag + f'error_coordinates: {error_coordinates}')
     return error_coordinates
 
 def sudoku_quadrant_from_coordinate(coordinate):
