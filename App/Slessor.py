35a535a9c683118b1641e21b29bea7a34e1e3fb2
Robin Spinks <uglybluecat@googlemail.com>
2026-06-24 11:42:51 +0100

    Add function to calculate quadrant from coordinate

--- App/Slessor.py
+++ App/Slessor.py
@@ -8,6 +8,14 @@ def sudoku_quadrant_checker(sudoku_board):
     error_coordinates += sudoku_row_checker(sudoku_board)
     print(tag + f'error_coordinates: {error_coordinates}')
 
+    for coordinate in error_coordinates:
+        print(tag + f'coordinate: {coordinate}')
+        error_quadrants.append(sudoku_quadrant_from_coordinate(coordinate))
+    print(tag + f'error_quadrants: {error_quadrants}')
+
+    if len(error_quadrants) > 0:
+        response = str(error_quadrants)
+    print(tag + f'response: {response}')
 
     return response
 
@@ -38,4 +46,42 @@ def sudoku_row_checker(sudoku_board):
     print(tag + f'error_coordinates: {error_coordinates}')
     return error_coordinates
 
+def sudoku_quadrant_from_coordinate(coordinate):
+    tag = 'sudoku_quadrant_from_coordinate | '
+    row = coordinate[0]
+    column = coordinate[1]
+    print(tag + f'coordinate: {coordinate} row: {row} column: {column}')
 
+    if row/3 <= 1:
+        print(tag + f'row/3 ({row/3}) <= 1')
+        if column/3 <= 1:
+            print(tag + f'column/3 ({column/3}) <= 1')
+            return 1
+        elif column/3 <= 2:
+            print(tag + f'1 < column/3 ({column/3}) <= 2')
+            return 2
+        else:
+            print(tag + f'2 < column/3 ({column/3})')
+            return 3
+    elif row/3 <= 2:
+        print(tag + f'1 < row/3 ({row/3}) <= 2')
+        if column/3 <= 1:
+            print(tag + f'column/3 ({column/3}) <= 1')
+            return 4
+        elif column/3 <= 2:
+            print(tag + f'1 < column/3 ({column/3}) <= 2')
+            return 5
+        else:
+            print(tag + f'2 < column/3 ({column/3})')
+            return 6
+    else:
+        print(tag + f'2 < row/3 ({row/3})')
+        if column/3 <= 1:
+            print(tag + f'column/3 ({column/3}) <= 1')
+            return 7
+        elif column/3 <= 2:
+            print(tag + f'1 < column/3 ({column/3}) <= 2')
+            return 8
+        else:
+            print(tag + f'2 < column/3 ({column/3})')
+            return 9
