a2acfce9e342402d901117ead23a7d170e17fd92
Robin Spinks <uglybluecat@googlemail.com>
2026-06-24 11:42:13 +0100

    Refactor row checking

--- App/Slessor.py
+++ App/Slessor.py
@@ -1,28 +1,41 @@
 def sudoku_quadrant_checker(sudoku_board):
-    print(f'sudoku_board: {sudoku_board}')
-    response = ""
+    tag = 'sudoku_quadrant_checker | '
+    print(tag + f'sudoku_board: {sudoku_board}')
+    response = "legal"
+    error_coordinates = []
+    error_quadrants = []
+
+    error_coordinates += sudoku_row_checker(sudoku_board)
+    print(tag + f'error_coordinates: {error_coordinates}')
+
+
+    return response
+
+def sudoku_row_checker(sudoku_board):
+    tag = 'sudoku_row_checker | '
+    error_coordinates = []
 
     for row in sudoku_board:
-        print(f'row: {row}')
-        row_list = row.split('(')[1].split(')')[0].split(',')
-        print(f'row_list: {row_list}')
-        error_locations = []
+        print(tag + f'row: {row}')
+        row_list = row.strip('()').split(',')
+        print(tag + f'row_list: {row_list}')
 
         for i in range(1,10):
             i_count = row_list.count(str(i))
-            print(f'i: {i} i_count: {i_count}')
+            print(tag + f'i: {i} i_count: {i_count}')
 
             if i_count > 1:
                 row_index = sudoku_board.index(row)
-                print(f'row_index: {row_index}')
+                print(tag + f'row_index: {row_index}')
 
                 prev_column_index = 0
                 for j in range (i_count):
                     column_index = row_list.index(str(i), prev_column_index)
-                    print(f'column_index: {column_index}')
+                    print(tag + f'column_index: {column_index}')
                     prev_column_index = column_index + 1
-                    error_locations.append([row_index + 1, column_index + 1])
+                    error_coordinates.append([row_index + 1, column_index + 1])
 
-                print(f'error_locations: {error_locations}')
+    print(tag + f'error_coordinates: {error_coordinates}')
+    return error_coordinates
 
-    return response
+
